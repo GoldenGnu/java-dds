8f371fedc57363502c8ccda1f1a7b4b8c102a7ec
niklaskr@gmail.com <niklaskr@gmail.com@d636fd90-2ee7-16c3-bf09-472137a6b9a8>
2011-09-24 09:13:45 +0000

    minor optimizing (Much more to be done...)

--- trunk/src/main/java/net/nikr/dds/DDSLineReader.java
+++ trunk/src/main/java/net/nikr/dds/DDSLineReader.java
@@ -61,6 +61,8 @@ public class DDSLineReader {
 			case ATI2:
 				readATI(stream, ddsHeader, banks, imageIndex);
 				break;
+			case NOT_DDS:
+				throw new IOException("Not a supported format!");
 			default:
 				throw new IOException(ddsHeader.getPixelFormat().getFormat().getName()+" is not a supported format!");
 		}
@@ -73,9 +75,9 @@ public class DDSLineReader {
 	}
 	
 	private void readUncompressed(ImageInputStream stream, DDSHeader ddsHeader, byte [][] banks, int imageIndex) throws IOException{
+		long amask, rmask, gmask, bmask, pixel, a, r, g, b;
 		for (int x = 0; x < ddsHeader.getWidth(imageIndex); x++) {
-
-			long pixel = 0;
+			pixel = 0;
 			switch ((int)ddsHeader.getPixelFormat().getRgbBitCount()){
 				case 8:
 					pixel = stream.readByte();
@@ -97,15 +99,15 @@ public class DDSLineReader {
 					break;
 			}
 			DDSPixelFormat pf = ddsHeader.getPixelFormat();
-			long amask = pf.getAlphaBitMask() >> pf.getAlphaShift() << (8 - pf.getAlphaBits());
-			long rmask = pf.getRedBitMask() >> pf.getRedShift() << (8 - pf.getRedBits());
-			long gmask = pf.getGreenBitMask() >> pf.getGreenShift() << (8 - pf.getGreenBits());
-			long bmask = pf.getBlueBitMask() >> pf.getBlueShift() << (8 - pf.getBlueBits());
+			amask = pf.getAlphaBitMask() >> pf.getAlphaShift() << (8 - pf.getAlphaBits());
+			rmask = pf.getRedBitMask() >> pf.getRedShift() << (8 - pf.getRedBits());
+			gmask = pf.getGreenBitMask() >> pf.getGreenShift() << (8 - pf.getGreenBits());
+			bmask = pf.getBlueBitMask() >> pf.getBlueShift() << (8 - pf.getBlueBits());
 			
-			long a = 255;
-			long r = 255;
-			long g = 255;
-			long b = 255;
+			a = 255;
+			r = 255;
+			g = 255;
+			b = 255;
 
 			if ((pf.getRgbBitCount() == 32)
 					&& (pf.getRedBitMask() == 0x3ff00000l)
@@ -145,14 +147,20 @@ public class DDSLineReader {
 		if (lineNumber == 0){
 			//System.out.println("Read line: "+y);
 			linesColor = new byte[LINES_PER_READ][(int)Math.max(ddsHeader.getWidth(imageIndex), 8)][COLORS_PER_READ];
-			for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
-				if (ddsHeader.getPixelFormat().isDXT3()){
+			if (ddsHeader.getPixelFormat().isDXT3()){
+				for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
 					decodeDXT3AlphaBlock(stream, ddsHeader, x);
+					decodeColorBlock(stream, ddsHeader, banks, x);
 				}
-				if (ddsHeader.getPixelFormat().isDXT5()){
+			} else if (ddsHeader.getPixelFormat().isDXT5()){
+				for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
 					decodeAtiAndDxt5AlphaBlock(stream, ddsHeader, x, BANK_ALPHA);
+					decodeColorBlock(stream, ddsHeader, banks, x);
 				}
-				decodeColorBlock(stream, ddsHeader, banks, x);
+			} else {
+				for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
+					decodeColorBlock(stream, ddsHeader, banks, x);
+				}
 			}
 		}
 		for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x++) {
@@ -168,8 +176,8 @@ public class DDSLineReader {
 		if (lineNumber >= LINES_PER_READ) lineNumber = 0;
 		if (lineNumber == 0){
 			linesColor = new byte[LINES_PER_READ][(int)Math.max(ddsHeader.getWidth(imageIndex), 8)][COLORS_PER_READ];
-			for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
-				if (ddsHeader.getPixelFormat().isATI1()){
+			if (ddsHeader.getPixelFormat().isATI1()){
+				for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
 					decodeAtiAndDxt5AlphaBlock(stream, ddsHeader, x, BANK_RED);
 					for (int yi = 0; yi < 4 ; yi++){
 						for (int xi = 0; xi < 4; xi++){
@@ -179,7 +187,9 @@ public class DDSLineReader {
 						}
 					}
 				}
-				if (ddsHeader.getPixelFormat().isATI2()){
+			}
+			if (ddsHeader.getPixelFormat().isATI2()){
+				for (int x = 0; x < (ddsHeader.getWidth(imageIndex)); x = x + 4) {
 					decodeAtiAndDxt5AlphaBlock(stream, ddsHeader, x, BANK_GREEN);
 					decodeAtiAndDxt5AlphaBlock(stream, ddsHeader, x, BANK_RED);
 					for (int yi = 0; yi < 4 ; yi++){
@@ -304,9 +314,6 @@ public class DDSLineReader {
 	}
 	
 	private void decodeAtiAndDxt5AlphaBlock(ImageInputStream stream, DDSHeader ddsHeader, int x, int bank) throws IOException{
-		//debug:
-		boolean debug = false; //(x == 192 && y == 96);
-		
 		int color0, color1, bits0, bits1, bits2, bits3, bits4, bits5;
 		int[] color = new int[8];
 		int[] bits = new int[6];
@@ -344,7 +351,6 @@ public class DDSLineReader {
 		//alpha[1] = (int) (alpha1 * (1.0/255.0));
 
 		if (color0 > color1){
-			if (debug) System.out.println("alpha0 > alpha1");
 			color[2] = (6*color[0] + 1*color[1])/7;
             color[3] = (5*color[0] + 2*color[1])/7;
             color[4] = (4*color[0] + 3*color[1])/7;
@@ -352,7 +358,6 @@ public class DDSLineReader {
             color[6] = (2*color[0] + 5*color[1])/7;
             color[7] = (1*color[0] + 6*color[1])/7;
 		} else {
-			if (debug) System.out.println("alpha0 <= alpha1");
 			color[2] = (4*color[0] + 1*color[1])/5;
 			color[3] = (3*color[0] + 2*color[1])/5;
 			color[4] = (2*color[0] + 3*color[1])/5;
@@ -360,23 +365,20 @@ public class DDSLineReader {
 			color[6] = 0;
 			color[7] = 255;
 		}
-		
+		int i, bit;
+		byte code;
 		for (int yi = 0; yi < 4 ; yi++){
 			for (int xi = 0; xi < 4; xi++){
 				//Calculate the bit posistion in the alpha0, alpha1 (long)
 				
-				int i = (3*(4*yi+xi)); //64bit UNSIGNED LONG: bit position
+				i = (3*(4*yi+xi)); //64bit UNSIGNED LONG: bit position
 				
-				int bit = (int) Math.floor(i / 8.0);  //where in the bits array to find the bit position
+				bit = (int) Math.floor(i / 8.0);  //where in the bits array to find the bit position
 				i = i - (bit * 8); //offset from the 64bit position to the bits array
 
 				//Extract 2bits, from the bits array
-				byte code = (byte)((bits[bit] >> i) & 7);
+				code = (byte)((bits[bit] >> i) & 7);
 				
-				//Debug
-				if (debug){
-					System.out.println("yi: "+yi+" xi: "+xi+" byte: "+(i*3)+" code: "+code+" alpha[code]: "+color[code]+" ("+Integer.toBinaryString(color[code])+")");
-				}
 				//Add the value to the alpha map
 				linesColor[yi][x+xi][bank] = (byte) color[code];
 			}
